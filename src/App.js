022ee068bf7cb38b04de80f473ba85d93f1485dc
Mugil99 <mugilya99@gmail.com>
2023-05-31 19:56:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'; 
+import TableRow from './Components/TableRow';
+import './styles.css'
 
-function App() {
+const App = () => {
+  const [coinData, setCoinData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false');
+        setCoinData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Coin Market Data</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>ID</th>
+            <th>Image</th>
+            <th>Symbol</th>
+            <th>Current Price</th>
+            <th>Total Volume</th>
+          </tr>
+        </thead>
+        <tbody>
+          {coinData.map((coin) => (
+            <TableRow key={coin.id} coin={coin} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
